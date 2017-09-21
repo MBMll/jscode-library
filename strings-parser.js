5b75e8350cef53416b7e53f4ad21ec893ba35e8e
talor.alue <taylor.xia@foxmail.com>
2017-09-21 17:53:04 +0800

    Update strings-parser.js

--- strings-parser.js
+++ strings-parser.js
@@ -1,1 +1,12 @@
-JSON.parse(decodeURI((location.search.replace(/^\?/g,'{"').replace(/\&/g,'","').replace(/\=/g, '":"')||'{"":"')+'"}'))
+//解析URL参数，直接替换成json格式的对象
+JSON.parse(
+  decodeURI(
+    (location
+     .search
+     .replace(/^\?/g,'{"')
+     .replace(/\&/g,'","')
+     .replace(/\=/g, '":"')
+     ||'{"":"')//防止空参数导致json解析错误
+    +'"}'
+  )
+)
